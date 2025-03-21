439ee97584a4e8aa0a2605ee1eb78341e21a5646
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-03-21 14:13:32 +0700

    Menambahkan event handler Form1_Load dan memanggil LoadData

--- Form1.cs
+++ Form1.cs
@@ -19,9 +19,14 @@ namespace CRUDSederhana
         {
             InitializeComponent();
         }
+
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
     }
-}
+    }
