90d91cda50f2c6e877927ddff3dcf3665e8f9a48
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-03-21 14:02:15 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDSederhana
+{
+    public partial class Form1 : Form
+    {
+        static string connectionString = string.Format(
+            "Server=127.0.0.1; database = organisasiMahasiswa; UID = root; Password = mariskacan10");
+    }
+}
